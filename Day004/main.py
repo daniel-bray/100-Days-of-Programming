3893476b01584f6833ba0ebbdc232f3290cac35e
Daniel J. Bray <braydj@gmail.com>
2022-09-21 16:32:42 -0500

    finished day 4

---
+++ Day004/main.py
@@ -0,0 +1,51 @@
+import random
+
+
+rock = """
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+"""
+
+paper = """
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+"""
+
+scissors = """
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+
+# Write your code below this line 👇
+options = [rock, paper, scissors]
+player_choice = int(
+    input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. ")
+)
+computer_choice = random.randint(0, 2)
+
+print(options[player_choice])
+print("Computer chose:")
+print(options[computer_choice])
+
+if player_choice == computer_choice:
+    print("Game is a draw.")
+elif (
+    (player_choice == 0 and computer_choice == 1)
+    or (player_choice == 1 and computer_choice == 2)
+    or (player_choice == 2 and computer_choice == 0)
+):
+    print("You lose.")
+else:
+    print("You win.")
