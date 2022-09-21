45ef08a2eadd958ec06a7fccf0bee595a7172957
Daniel J. Bray <braydj@gmail.com>
2022-09-21 16:54:56 -0500

    edited solution for day 4

--- Day004/main.py
+++ Day004/main.py
@@ -31,21 +31,24 @@ scissors = """
 # Write your code below this line 👇
 options = [rock, paper, scissors]
 player_choice = int(
-    input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. ")
+    input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
 )
 computer_choice = random.randint(0, 2)
 
-print(options[player_choice])
-print("Computer chose:")
-print(options[computer_choice])
-
-if player_choice == computer_choice:
-    print("Game is a draw.")
-elif (
-    (player_choice == 0 and computer_choice == 1)
-    or (player_choice == 1 and computer_choice == 2)
-    or (player_choice == 2 and computer_choice == 0)
-):
-    print("You lose.")
+if player_choice < 0 or player_choice > 2:
+    print("You chose an invalid number. You lose.")
 else:
-    print("You win.")
+    print(options[player_choice])
+    print("Computer chose:")
+    print(options[computer_choice])
+
+    if player_choice == computer_choice:
+        print("Game is a draw.")
+    elif (
+        (player_choice == 0 and computer_choice == 1)
+        or (player_choice == 1 and computer_choice == 2)
+        or (player_choice == 2 and computer_choice == 0)
+    ):
+        print("You lose.")
+    else:
+        print("You win.")
